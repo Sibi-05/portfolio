3388443a7a35c36afd7876af83439316169a8652
sibi <ssibi769@gmail.com>
2025-07-31 15:00:54 +0530

    Changes in button

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -3,7 +3,7 @@ const projects = [
     title: "E-commerce Website",
     description:
       "A fully responsive e-commerce website with product listings, cart functionality, and smooth user experience.",
-    tech: ["React", "Tailwind CSS", "Node.js", "Express", "MongoDB"," Redux"],
+    tech: ["React", "Tailwind CSS", "Node.js", "Express", "MongoDB", "Redux"],
     link: "https://main--mr51b1-ecommerce.netlify.app/",
   },
   {
@@ -32,33 +32,36 @@ const projects = [
 const Projects = () => (
   <div className="p-10 text-gray-800 max-w-5xl mx-auto">
     <h1 className="text-3xl font-bold mb-4 text-center">Projects</h1>
-    <p className="text-center mb-8 text-gray-600 ">
+    <p className="text-center mb-8 text-gray-600">
       Here are some of the projects I've built:
     </p>
 
-    <div className="grid gap-6 sm:grid-cols-2 ">
+    <div className="grid gap-6 sm:grid-cols-2">
       {projects.map((project, index) => (
         <div
-          key={index+1}
-          className="bg-white rounded-xl shadow-md p-6 transition hover:shadow-lg"
+          key={index}
+          className="bg-white rounded-xl shadow-md p-6 flex flex-col justify-between min-h-[300px]"
         >
-          <h2 className="text-xl font-semibold mb-2">{project.title}</h2>
-          <p className="text-gray-600 mb-4">{project.description}</p>
-          <div className="flex flex-wrap gap-2 mb-4">
-            {project.tech.map((tech, idx) => (
-              <span
-                key={idx+1}
-                className="bg-blue-100 text-black text-xs font-medium px-2.5 py-0.5 rounded"
-              >
-                {tech}
-              </span>
-            ))}
+          <div>
+            <h2 className="text-xl font-semibold mb-2">{project.title}</h2>
+            <p className="text-gray-600 mb-4">{project.description}</p>
+            <div className="flex flex-wrap gap-2 mb-4">
+              {project.tech.map((tech, idx) => (
+                <span
+                  key={idx}
+                  className="bg-blue-100 text-black text-xs font-medium px-2.5 py-0.5 rounded"
+                >
+                  {tech}
+                </span>
+              ))}
+            </div>
           </div>
+
           <a
             href={project.link}
             target="_blank"
             rel="noopener noreferrer"
-            className="text-sm text-black border-2 flex justify-center w-30 mt-2 bg-transparent hover:bg-black hover:text-white transition px-4 py-2 rounded-xl"
+            className="text-sm text-black border-2 w-32 text-center bg-transparent hover:bg-black hover:text-white transition px-1 py-2 rounded-xl mt-auto"
           >
             Live Preview
           </a>
